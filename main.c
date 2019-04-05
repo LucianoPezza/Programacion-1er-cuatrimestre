1a1ff7e1ff0e684551fc58e250e5e2f8a08550b0
LucianoPezza <47321345+LucianoPezza@users.noreply.github.com>
2019-04-05 12:24:11 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+char nombre[6];
+char otroNombre[50]="Jose Perez";
+//int largo;
+int compara;
+//strcpy(nombre, otroNombre);
+strlwr(otronombre);
+strcat(nombre,otroNombre);
+strncpy(nombre, otroNombre,5;
+printf("Su nombre es: %s", nombre);
+
+compara = strcmp(nombre, "pepe");
+//printf("Ingrese su nombre: ");
+//fgets(nombre, 50, stdin);
+//scanf("%[^\a]", nombre);
+//gets(nombre);
+
+//largo = strlen(nombre);
+//printf("\n%d",largo);
+
+//printf("%d",compara);
+/*if(nombre == "pepe")
+{
+
+printf("Usted se llama pepe");
+
+}
+*/
+return 0;
+}
