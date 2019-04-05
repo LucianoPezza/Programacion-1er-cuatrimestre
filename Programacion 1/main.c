9af3e08485a4148b2dee22a05838d0793a2be835
LucianoPezza <47321345+LucianoPezza@users.noreply.github.com>
2019-04-05 12:13:22 -0300

    Add files via upload

--- Programacion 1/main.c
+++ Programacion 1/main.c
@@ -1,37 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main()
 {
-    int numero;
-    int numero1;
-    char letra;
-    char palabra[10];
+char nombre[6];
+char otroNombre[50]="Jose Perez";
+//int largo;
+int compara;
+//strcpy(nombre, otroNombre);
+strncpy(nombre, otroNombre,5;
+printf("Su nombre es: %s", nombre);
 
-    printf("Ingrese un numero\n");
-    scanf("%d",&numero);
+compara = strcmp(nombre, "pepe");
+//printf("Ingrese su nombre: ");
+//fgets(nombre, 50, stdin);
+//scanf("%[^\a]", nombre);
+//gets(nombre);
 
-    printf("Ingrese numero1\n");
-    scanf("%d",&numero1);
+//largo = strlen(nombre);
+//printf("\n%d",largo);
 
-    printf("Ingrese una letra\n");
-    fflush(stdin);//* una de las soluciones era tambien agregarle un espacio al "%c" provocando asi que al presionar Enter no terminara el proceso(la funcion en linux se llama setbuf(stdin,null))"
-    scanf("%c",&letra);
+//printf("%d",compara);
+/*if(nombre == "pepe")
+{
 
-    printf("Ingrese una palabra\n");
-    scanf("\n %s",palabra);//no se utiliza el ampersan (&) por que es una cadena de caracteres
+printf("Usted se llama pepe");
 
-    int suma = numero + numero1;
-
-    printf("\nEl numero es: %d\nEl numero1 es: %d\nLa letra es: %c\nY la suma de los numeros es: %d\nLa palabra es:%s\n", numero,numero1,letra,suma,palabra);
-
-
-    system("pause");//*sirve para que el ejecutable que se encuentra en bin se pause*//
-
-
-
-
-
-
-    return 0;
-    }
+}
+*/
+return 0;
+}
