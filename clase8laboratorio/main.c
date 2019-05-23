e60b4bb3da161a78a832d004472d6a95c7421e84
LucianoPezza <47321345+LucianoPezza@users.noreply.github.com>
2019-05-23 10:20:25 -0300

    Add files via upload

---
+++ clase8laboratorio/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "biblioteca.h"
+#define CANT 2
+
+int main()
+{
+    int i;
+    int* pArrayDePersona;
+
+    ePersona lista;
+
+    dameUnaPersona(lista);
+
+    mostrarUnaPersona(lista);
+
+    for(i=0;i<CANT;i++)
+    {
+
+
+    }
+
+    return 0;
+}
